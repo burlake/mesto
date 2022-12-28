e6c3e357b11de6569a3f415bf584391cf36baff2
Elizaveta Burlakova <es.burlakova@mail.ru>
2022-12-29 01:13:46 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,6 @@
-console.log('Hello world');
+const aboutButton = document.querySelector('.action-button');
+const aboutPopup = document.querySelector('.popup_about');
+
+aboutButton.addEventListener('click', (event) => {
+    aboutPopup.classList.add('popup_opend');
+})
