7b8ffb5408572c799f84641999dffd9f7df2082a
Elizaveta Burlakova <es.burlakova@mail.ru>
2022-12-29 01:25:32 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -4,5 +4,5 @@ const aboutPopup = document.querySelector('.popup_about');
 aboutButton.addEventListener('click', (event) => {
     event.preventDefault();
     console.log(event);
-    aboutPopup.classList.add('.popup_opend');
+    aboutPopup.classList.add('popup_opend');
 })
