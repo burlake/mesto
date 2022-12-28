8a68b1b276a61415e44088686ef8e3de98fff507
Elizaveta Burlakova <es.burlakova@mail.ru>
2022-12-29 02:09:21 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -16,7 +16,11 @@ aboutCloseButton.addEventListener('click', (event) => {
 
 aboutPopup.addEventListener('click', (event) => {
     event.preventDefault();
-    if(event.target === event.currentTarget);{
-        aboutPopup.classList.remove('popup_opened')
+    console.log(event.targe, event.currentTarget);
+    //aboutPopup.classList.remove('popup_opened')
+
+
+   // if(event.target === event.currentTarget);{
+        //aboutPopup.classList.remove('popup_opened')
     };
 })
